853793ca46c009e4fdf922b8c7ef58aaeb956d61
Alexander Lam <alam@proteus.com>
2017-11-22 09:47:27 -0800

    Corrected calculation of average revenue change

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,6 +18,15 @@ hiMonth = ''
 loMonth = ''
 hiRevenue = 0
 loRevenue = 0
+differences = []
+prevRevenue = 0
+
+# average solver
+def average(arg):
+    total = 0
+    for item in arg:
+        total += item
+    return (total/len(arg))
 
 # open file and extract data
 with open(filePath,'r',newline='') as fileData:
@@ -27,12 +36,16 @@ with open(filePath,'r',newline='') as fileData:
         # print(row)
         totalRevenue += int(row[1])
         nMonths += 1 #assumes each row in the file is a unique month
+        differences.append(int(row[1]) - prevRevenue)
+        prevRevenue = int(row[1]) 
         if int(row[1]) > hiRevenue:
             hiRevenue = int(row[1])
             hiMonth = row[0]
         if int(row[1]) < loRevenue:
             loRevenue = int(row[1])
             loMonth = row[0]
+# cleanup of differences
+differences = differences[1:]
 
 
 
@@ -42,7 +55,7 @@ outputText.append('Financial Analysis')
 outputText.append('---------------------------------')
 outputText.append('Total Months: ' + str(nMonths))
 outputText.append('Total Revenue: $' + str(totalRevenue//nMonths))
-outputText.append('Average Revenue Change: $' + str(totalRevenue))
+outputText.append('Average Revenue Change: $' + str(round(average(differences),2)))
 outputText.append('Greatest Increase in Revenue: ' + hiMonth + ' ($' + str(hiRevenue) + ')') 
 outputText.append('Greatest Decrease in Revenue: ' + loMonth + ' ($' + str(loRevenue) + ')')
 outputText.append('\n \n \nSource file: ' + filePath)
